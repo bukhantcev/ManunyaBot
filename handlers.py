540747675aec202dfd18de0d92d70918637fb639
Alexander Bukhantsev <bukhantcev@mail.ru>
2023-11-27 00:29:29 +0300

    manunya1

--- handlers.py
+++ handlers.py
@@ -1,6 +1,10 @@
-from loader import dp
+import os
+
+from loader import dp, bot
 from aiogram.types import Message, CallbackQuery
 from keyboards import kb_folders
+from aiogram.types import InputFile
+
 
 
 
@@ -14,8 +18,13 @@ async def go(message: Message):
 async def get_file(cb: CallbackQuery):
     try:
         name = cb.data
+        for folder in os.listdir('materials'):
+            if folder == name:
+                for file in os.listdir(f'materials/{folder}'):
+                    file_input = InputFile(f'materials/{folder}/{file}')
+                    await bot.send_document(chat_id=cb.message.chat.id, document=file_input)
         
 
 
     except:
-        pass
+        await cb.answer(text="Залупу!")
