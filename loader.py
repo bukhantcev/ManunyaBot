2741d52c0523178fab87fec03187b9cb41d33025
bukhantcev <bukhantcev@mail.ru>
2023-12-11 23:40:03 +0300

    11.12.23

--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 memory = MemoryStorage()
 
 
-#bot = Bot(os.getenv('TOKEN'))
-bot = Bot('6283280993:AAHi8EqmQ41zE8rLl_0ayUexaX69DlCxs20')
+bot = Bot(os.getenv('TOKEN'))
+#bot = Bot('6283280993:AAHi8EqmQ41zE8rLl_0ayUexaX69DlCxs20')
 
 dp = Dispatcher(bot, storage=memory)
