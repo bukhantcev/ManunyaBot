ee1f8c81189d8009c0d33cdbc9bc663980482a29
buha <bukhantcev@mail.ru>
2024-10-09 08:03:59 +0300

    Rostov

--- loader.py
+++ loader.py
@@ -7,6 +7,6 @@ memory = MemoryStorage()
 
 
 bot = Bot(os.getenv('TOKEN'))
-#bot = Bot('6283280993:AAHi8EqmQ41zE8rLl_0ayUexaX69DlCxs20')
+# bot = Bot('6283280993:AAHi8EqmQ41zE8rLl_0ayUexaX69DlCxs20')
 
 dp = Dispatcher(bot, storage=memory)
